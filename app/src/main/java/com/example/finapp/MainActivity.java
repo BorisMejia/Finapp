0870d03a07bc9925ae5f5c3719b0a2f0e42a3057
BorisFer <borisfernandez916@gmail.com>
2022-12-06 11:44:22 -0500

    Interface

--- app/src/main/java/com/example/finapp/MainActivity.java
+++ app/src/main/java/com/example/finapp/MainActivity.java
@@ -6,19 +6,34 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 public class MainActivity extends AppCompatActivity {
 
     TextView txtLogIn;
+    EditText txtName, txtEmail, txtPassword;
+    Button btnSingUp;
+
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        txtName = (EditText) findViewById(R.id.txtName);
+        txtEmail = (EditText) findViewById(R.id.txtEmail);
+        txtPassword = (EditText) findViewById(R.id.txtPassword);
         txtLogIn = (TextView) findViewById(R.id.txtLogIn);
+        btnSingUp = (Button) findViewById(R.id.btnSignUp);
 
         txtLogIn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,6 +42,14 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intento);
             }
         });
+        btnSingUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        //inicializarFirebase();
+
     }
 
 
